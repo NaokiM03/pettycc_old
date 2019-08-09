9999c2f6319a17007da45c383a3b8c1f395a7f63
NaokiM03 <mattsun.phone@gmail.com>
2019-08-09 16:36:03 +0900

    ADD error message at wrong place

--- pettycc.rb
+++ pettycc.rb
@@ -5,13 +5,14 @@ module TokenKind
 end
 
 class Token
-  attr_accessor :kind, :next, :val, :str
+  attr_accessor :kind, :next, :val, :str, :cur
 
   def initialize
     @kind = nil
     @next = nil
     @val  = nil
     @str  = nil
+    @cur  = nil
   end
 end
 
@@ -20,8 +21,21 @@ def error(msg)
   exit!
 end
 
-def int? str
+def error_at(msg)
+  pos = $user_input_cur
+  STDERR.puts("#{ARGV[0]}\n" + "#{" "*$token.cur + "^ " + msg}\n")
+  exit!
+end
+
+def int?(str)
   str.to_i.to_s == str.to_s
+end
+
+def next_cur(p)
+  str = p.slice!(0)
+  return if str.nil?
+  $user_input_cur += 1
+  return str
 end
 
 def consume(op)
@@ -34,14 +48,14 @@ end
 
 def expect(op)
   if $token.kind != TokenKind::RESERVED || $token.str != op
-    STDERR.puts("expected '#{op}'")
+    error_at("expected '#{op}'")
   end
   $token = $token.next
 end
 
 def expect_number
   if $token.kind != TokenKind::NUM
-    STDERR.puts("expected a number")
+    error_at("expected a number")
   end
   val = $token.val
   $token = $token.next
@@ -56,36 +70,38 @@ def new_token(kind, cur, str)
   tok = Token.new
   tok.kind = kind
   tok.str = str
+  tok.cur = $user_input_cur
   cur.next = tok
   return tok
 end
 
-def tokenize(p)
+def tokenize()
+  p = $user_input.dup
   cur = Token.new
   $token = cur
 
   while p.length != 0
     if p[0] == " "
-      p.slice!(0)
+      next_cur(p)
       next
     end
 
     if p[0] == "+" || p[0] == "-"
-      cur = new_token(TokenKind::RESERVED, cur, p.slice!(0))
+      cur = new_token(TokenKind::RESERVED, cur, next_cur(p))
       next
     end
 
     if int?(p[0])
       num = ""
       while int?(p[0])
-        num += p.slice!(0)
+        num += next_cur(p)
       end
       cur = new_token(TokenKind::NUM, cur, num)
       cur.val = num.to_i
       next
     end
 
-    error("invalid token")
+    error_at("invalid token")
   end
 
   new_token(TokenKind::EOF, cur, p[0])
@@ -93,13 +109,17 @@ def tokenize(p)
 end
 
 $token
+$user_input
+$user_input_cur
 
 def main
   if ARGV.length != 1
     error "#{$0}: invalid number of arguments\n"
   end
 
-  tokenize(ARGV[0].dup)
+  $user_input = ARGV[0]
+  $user_input_cur = -1
+  $token = tokenize()
 
   puts(".intel_syntax noprefix\n")
   puts(".global main\n")
@@ -120,5 +140,5 @@ def main
 end
 
 if __FILE__ == $0
-  main
+  main()
 end
